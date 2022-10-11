26421152f4fac561bb15bf561416a25133035851
Jose Carrera <jose.carrera@amido.com>
2022-10-11 10:53:04 +0100

    adds missing packages

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getIframeBody', () => {
+    // get the iframe > document > body
+    // and retry until the body element is not empty
+    cy.log('getIframeBody')
+  
+    return cy
+    .get('iframe[data-cy="the-frame"]', { log: false })
+    .its('0.contentDocument.body', { log: false }).should('not.be.empty')
+    // wraps "body" DOM element to allow
+    // chaining more Cypress commands, like ".find(...)"
+    // https://on.cypress.io/wrap
+    .then((body) => cy.wrap(body, { log: false }))
+  })
+  
